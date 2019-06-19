0c5b7e68de5e03041e4189e891e379d07eb11f71
chidera <dexiouz@gmail.com>
2019-06-19 11:36:14 +0100

    Added todo.js and todoItems.js

---
+++ src/Todo.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+class Todo extends Component {
+    render(){
+    return (
+      <h1> Welcome to my todo App </h1>
+    )
+  }
+}
+export default Todo;
---
+++ src/TodoItems.js
@@ -0,0 +1,7 @@
+import React, { Component } from "react";
+
+const TodoItems = () => {
+  return <h2>I render individul todo Items</h2>;
+};
+
+export default TodoItems;
