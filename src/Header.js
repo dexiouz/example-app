b62550783728cdb6db7590ff673eceb8b8cff732
chidera <dexiouz@gmail.com>
2019-06-19 12:16:51 +0100

    Added header

---
+++ src/Header.js
@@ -0,0 +1,6 @@
+import React, { Component } from "react";
+const Header = () => {
+  return <h2>TODO_APP</h2>;
+};
+
+export default Header;
--- src/Todo.js
+++ src/Todo.js
@@ -1,9 +1,13 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import Header from "./Header";
 class Todo extends Component {
-    render(){
+  render() {
     return (
-      <h1> Welcome to my todo App </h1>
-    )
+      <div>
+        <Header />
+        <h1> Welcome to my todo App </h1>
+      </div>
+    );
   }
 }
 export default Todo;
